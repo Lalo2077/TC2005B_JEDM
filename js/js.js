c28e544e4bee0db0b44f98e1340ccc725b7423c1
lalo <A01735676@tec.mx>
2022-08-14 18:03:27 -0500

    lab 3 y 4 actualizados y Lab 5 creado

---
+++ js/js.js
@@ -0,0 +1,104 @@
+// variables, constantes, consola (log, info, warn, error)
+
+//Forma moderna de declarar variables
+//El alcance es a nivel de bloque {}
+let streamer_españa = "Ibai";
+
+//Forma clásica de declarar variables
+//El alcance es a nivel de función 
+var streamer_noruega = "Rubius";
+
+const streamer_colombia = "JuanSGuarnizo";
+
+console.log(streamer_españa);
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+//La siguiente línea produce un error porque i murió en el 
+//bloque anterior
+//console.log(i);
+
+// for (var i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+// console.log(i);
+
+console.info("Esto es información");
+
+console.warn("bro?");
+
+console.error("Error");
+
+console.assert(streamer_españa === streamer_noruega);
+
+console.log(streamer_colombia);
+
+// alert, prompt, confirm
+
+alert("WTF AMIGO");
+
+const nombre = prompt("¿Cómo te llamas");
+
+console.log("Hola " + nombre);
+
+const is_hungry = confirm( "¿Tienes hambre?");
+
+// Funciones tradicionales
+
+function comer() {
+    const comida = "no se"
+    console.log(comida);
+    return true;
+}
+
+const comida = comer;
+
+if(is_hungry) {
+    alert("Come :D");
+    comida();
+} else {
+    alert("._.");
+}
+
+// Funciones modernas 
+
+const anon = () => console.log("anónimo");
+
+anon();
+
+
+//arreglos
+
+const menu = ["Papa al horno", "Arrachera","Tamales"];
+
+console.log(menu);
+
+for(let p in menu) {
+    console.log(p);
+}
+
+for(let p of menu) {
+    console.log(p);
+}
+
+menu[10] = "Pasta";
+console.log(menu);
+
+menu.length = 2;
+console.log(menu);
+
+
+
+//html dinámico con eventos 
+const boton = document.getElementById("boton_imagen");
+
+boton.onclick = () => {
+    console.log("Click!");
+    document.getElementById("contenedor_imagen").innerHTML =
+        '<img src="media/nutria.jpeg" alt="Juan nos cuida">';
+}
+
+
