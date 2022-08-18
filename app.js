a277fbd395966fbe64d0214f6d47efcd5f737fc6
lalo <A01735676@tec.mx>
2022-08-18 12:37:51 -0500

    Laboratorio 7 setTimeout

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
+setTimeout(() => console.log("código asíncrono"), 4000);
+setTimeout(() => console.log("ERROR: System Failure"), 8000);
+
 const filesystem = require('fs');
 filesystem.writeFileSync('hola.txt', 'Hola desde node');
 
 console.log("Hola desde node");
+
+const arreglo = [5000,60,90,100,10,20,10000,0,120,2000,340,1000,50,20000];
+
+//Imprime el arreglo en orden
+//Porque se ejecuta con los timeouts de los valores del arreglo
+for (let item of arreglo) {
+    setTimeout(() => {
+        console.log(item);
+    }, item);
+}
