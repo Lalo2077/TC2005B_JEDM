e3c4084d35f59baef8d8d8fcf8d90d470df25a10
lalo <A01735676@tec.mx>
2022-08-23 12:52:27 -0500

    Manejrar rutas GET y POST

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 //Middleware
 app.use((request, response, next) => {
@@ -9,12 +13,47 @@ app.use((request, response, next) => {
 
 app.use((request, response, next) => {
     console.log('Otro middleware sin reiniciar!');
-    response.send('¡Hola mundo!'); //Manda la respuesta
     next();
 });
 
-app.use((request, respones, next) => {
-    console.log("El papu middleware")
+app.get('/trivia', (request, response, next) => {
+    let html = '<!DOCTYPE html>' +
+    '<head><meta charset="UTF-8"></head>' +
+    "<h1>Pregunta a la papu nutria quién va a ganar</h1>" +
+    '<form action="trivia" method="POST">' +
+    '<fieldset>' +
+    '<legend>Equipos</legend>' +
+    '<label for ="visitantes">visitantes<input type="text" name="visitante" id="visitante">' +
+    '<br/><br/>' +
+    '<label for ="local">local<input type="text" name="local" id="local">' +
+    '</fieldset>' +
+    '<input type="submit" value="Adivina">' +
+    '</form>';
+    response.send(html); 
 });
+
+app.post('/trivia', (request, response, next) => {
+    console.log(request.body);
+    if (Math.floor(Math.random() * 2) == 0) {
+        response.send('<h2>' + request.body.visitante + '</h2>');
+    }else{
+        response.send('<h2>' + request.body.local + '</h2>');
+    }
+});
+
+app.use('/casa/nutria', (request, response, next) => {
+    response.send('¡Hola! desde la ruta "/casa/nutria"'); 
+});
+
+app.use('/casa', (request, response, next) => {
+    response.send('¡Hola! desde la ruta "/casa"'); 
+});
+
+app.use((request, response, next) => {
+    console.log("El papu middleware");
+    response.send('¡Hola mundo!'); //Manda la respuesta
+});
+
+
 
 app.listen(3000);
