39c4717a262b5ff3523e5c0c2f8588852bacd919
lalo <A01735676@tec.mx>
2022-08-25 00:26:40 -0500

    Laboratorio 10 ruta consultorio

--- laboratorios/app.js
+++ laboratorios/app.js
@@ -5,11 +5,11 @@ const { Console } = require('console');
 const filesystem = require('fs');
 filesystem.writeFileSync('hola.txt', 'Hola desde node');
 
-console.log("Hola desde node");
+//console.log("Hola desde node");
 
-const arreglo = [1,2,3,4,5,6];
+//const arreglo = [1,2,3,4,5,6];
 
-function Promedio(myArray) {
+/*function Promedio(myArray) {
     var i = 0, sum = 0, arreglo_length = myArray.length;
     while (i < arreglo_length) {
         sum = sum + myArray[i++];
@@ -28,6 +28,7 @@ for (let item of arreglo) {
         console.log(item);
     }, item);
 }
+*/
 
 const http = require('http');
 const { buffer } = require('stream/consumers');
@@ -45,9 +46,22 @@ const server = http.createServer( (request, response) => {
         response.write('<h1>La casa de las nutrias</h1>')
         response.write('</header>')
         response.write('<p> Hecho por Jose Eduardo Diaz Maldonado</p></header>');
-        response.write('<section><p> Bienvenido a la casa de las nutrias</P></br>');
-        response.write('<a href=http://localhost:3000/bar> Ir al papu bar </a>');
-        response.write('')
+        response.write('<section><p> Bienvenido a la casa de las nutrias</P><br>');
+        response.write('<a href=http://localhost:3000/bar span style="color: blue;"> Ir al papu bar -> </a>');
+        response.write('<br><br><a href=http://localhost:3000/consultorio span style="color: brown;"> Ir al consultorio -> </a>');
+        response.write('</body>')
+        response.end();
+
+    }else if (request.url === "/consultorio") {
+        response.write('<!DOCTYPE html>');
+        response.write('<html lang="en"><head>');
+        response.write('<title>Laboratorio10_A01735676</title>');
+        response.write('</head><body>');
+        response.write('<section><p> Este es el consultorio, pronto sera atendido</P></br>');
+        response.write('<img src="https://i.ytimg.com/vi/hmSFMOgnWNI/maxresdefault.jpg" width="600" heigth= "400" alt="consultorio"><br>');
+        response.write('<a href=https://www.youtube.com/watch?v=hmSFMOgnWNI> Aqui lo atenderan </a><br><br>');
+        response.write('<a href=http://localhost:3000/bar span style="color: blue;"> Si no te sirvio de nada la consulta, mejor desahoga tus penas en el papu bar -> </a>');
+        response.write('</body>')
         response.end();
 
     }else if (request.url === "/bar" && request.method === "GET") {
@@ -63,7 +77,8 @@ const server = http.createServer( (request, response) => {
         response.write('</fieldset>');
         response.write('<input type="submit" value="Ordenar">');
         response.write('</form>');
-        response.write('<br><br><a href=http://localhost:3000/casa> Volver a la casa de las nutrias </a>');
+        response.write('<br><img src="https://gastronomiaycia.republica.com/wp-content/uploads/2021/12/lista_bar_2021_paradise-680x447.jpg" width="400" heigth= "200" alt="papu bar">');
+        response.write('<br><br><a href=http://localhost:3000/casa span style="color: green;"> <- Volver a la casa de las nutrias </a>');
         response.end();
 
     }else if (request.url === "/bar" && request.method === "POST") {
@@ -78,28 +93,26 @@ const server = http.createServer( (request, response) => {
             console.log(datos_completos);
             const dato_orden = datos_completos.split('&')[0].split('=')[1];
             console.log(dato_orden);
+
+            filesystem.writeFileSync('pedidosPapuBar.txt', dato_orden); //el nombre del archivo es pedidosPapuBar.txt
             response.setHeader('Content-Type', 'text/html');
             response.write("<!DOCTYPE html>");
             response.write('<head><meta charset="UTF-8"></head>')
             if (dato_orden === "Cerveza") {
                 response.write('<h1>Aquí tiene su ' + dato_orden + '</h1>');
                 response.write('<img src="https://lalicorera.com/img/products/Corona-extra.png" width="200" heigth= "200" alt="cerveza">');
-                response.write('<br><br><a href=http://localhost:3000/bar> Volver al papu bar </a>');
             }else if(dato_orden === "Vino") {
                 response.write('<h1>Aquí tiene su ' + dato_orden + '</h1>');
                 response.write('<img src="https://pinamora.com/wp-content/uploads/2020/08/sandra-vino-blanco.png" width="200" heigth= "200" alt="vino">');
-                response.write('<br><br><a href=http://localhost:3000/bar> Volver al papu bar </a>');
             }else if (dato_orden === "Tequila") {
                 response.write('<h1>Aquí tiene su ' + dato_orden + '</h1>');
                 response.write('<img src="https://www.donjulio.com/static/images/product-blanco.png" width="200" heigth= "200" alt="tequila">');
-                response.write('<br><br><a href=http://localhost:3000/bar> Volver al papu bar </a>');
             }else {
                 response.write('<h1>No tenemos de ese hermano.</h1>');
                 response.write('<img src="https://pbs.twimg.com/media/FapRZItXgAMCxed?format=jpg&name=small" width="300" heigth= "300" alt="No tenemos">');
-                response.write('<br><br><a href=http://localhost:3000/bar> Volver al papu bar </a>');
 
             }
-            
+            response.write('<br><br><a href=http://localhost:3000/bar span style="color: blue;"> <- Volver al papu bar </a>');
             return response.end();
         });
     }
